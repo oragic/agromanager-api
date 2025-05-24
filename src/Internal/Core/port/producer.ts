f3aa41da2a7cd3aa4783d5081ebc21e890976dee
Rai D Alves <67238752+oragic@users.noreply.github.com>
2025-05-24 16:05:07 -0300

    refactor: :recycle: Refactor Producer repository port: Remove coments and add type in methods params

--- src/Internal/Core/port/producer.ts
+++ src/Internal/Core/port/producer.ts
@@ -3,9 +3,9 @@ import { ProdutorRural } from '../domain/Producer';
 export interface ProducerRepository {
   create(data: ProdutorRural): Promise<ProdutorRural | null>;
   //find(): Promise<ProdutorRural[]>;
-  findById(id): Promise<ProdutorRural | null>;
-  //update(id): Promise<void>;
-  //delete(id): Promise<void>;
+  findById(id: string): Promise<ProdutorRural | null>;
+  update(id: string): Promise<ProdutorRural | null>;
+  remove(id: string): Promise<boolean>;
 }
 
 export interface ProducerService {
