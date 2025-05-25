4e55fad02cb22ca6fef2b969b531eee0391e4e0b
Rai D Alves <67238752+oragic@users.noreply.github.com>
2025-05-25 19:25:54 -0300

    refactor: :fire: Remove not used files

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,6 +1,4 @@
 import { Module } from '@nestjs/common';
-import { AppController } from './Interface/Http/controllers/app.controller';
-import { AppService } from './app.service';
 import { ProducerController } from './Interface/Http/controllers/producer.controller';
 import { ProducerService } from './Internal/Core/service/producer.service';
 import { ProducerRepositoryAdapter } from './Internal/Adapters/repository/producer.repository';
@@ -8,9 +6,8 @@ import { PRODUCER_REPOSITORY } from './Internal/Core/port/producer.tokens';
 
 @Module({
   imports: [],
-  controllers: [AppController, ProducerController],
+  controllers: [ProducerController],
   providers: [
-    AppService,
     {
       provide: PRODUCER_REPOSITORY,
       useClass: ProducerRepositoryAdapter,
