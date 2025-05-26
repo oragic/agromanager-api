ea02523d826d4d09c00df8a68b1d717f8c84dcd8
Rai D Alves <67238752+oragic@users.noreply.github.com>
2025-05-26 11:10:09 -0300

    feat: :sparkles: Containerize the application

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,16 +1,32 @@
 import { Module } from '@nestjs/common';
 import { ProducerController } from './Interface/Http/controllers/producer.controller';
 import { ProducerService } from './Internal/Core/service/producer.service';
-import { ProducerRepositoryAdapter } from './Internal/Adapters/repository/producer.repository';
-import { PRODUCER_REPOSITORY } from './Internal/Core/port/producer.tokens';
+import { ProducerTypeOrmRepository } from './Internal/Adapters/repository/producer.repository';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { ProducerEntity } from './Infrastructure/entities/producer.entity';
+import { FarmEntity } from './Infrastructure/entities/farm.entity';
+import { CropEntity } from './Infrastructure/entities/crop.entity';
 
 @Module({
-  imports: [],
+  imports: [
+    TypeOrmModule.forRoot({
+      type: 'postgres',
+      host: process.env.DB_HOST,
+      port: Number(process.env.DB_PORT),
+      username: process.env.DB_USERNAME,
+      password: process.env.DB_PASSWORD,
+      database: process.env.DB_NAME,
+      autoLoadEntities: true,
+      entities: [ProducerEntity, FarmEntity, CropEntity],
+      synchronize: true,
+    }),
+    TypeOrmModule.forFeature([ProducerEntity]),
+  ],
   controllers: [ProducerController],
   providers: [
     {
-      provide: PRODUCER_REPOSITORY,
-      useClass: ProducerRepositoryAdapter,
+      provide: 'ProducerRepository',
+      useClass: ProducerTypeOrmRepository,
     },
     ProducerService,
   ],
